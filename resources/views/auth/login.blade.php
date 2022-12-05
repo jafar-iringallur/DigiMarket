f667b47f49f6dce46c9c3a27d1b4fab668a540ec
Jafar <jafar@sellowpay.com>
2022-12-05 15:54:28 +0530

    background color updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
 	</head>
-	<body>
+	<body style=" background-color: #f0f0f0;">
 	<section class="ftco-section">
 		<div class="container">
 			{{-- <div class="row justify-content-center">
