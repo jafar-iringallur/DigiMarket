c99afe31dc97155d29ad6b0eb454a21b67e039bb
Jafar <jafar@sellowpay.com>
2022-12-13 13:11:19 +0530

    dashboard template added

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
 });
 // Route::get('/getting-started', function () {
 //     return view('getting_started');
