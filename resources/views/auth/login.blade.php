c2bce2a768bff7245e86e2840eda679787db9b41
Jafar <jafar@sellowpay.com>
2022-12-13 13:43:43 +0530

    as

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,12 +25,12 @@
 				  <div class="row justify-content-center">
 					<div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 		
-					  <div class="d-flex justify-content-center py-4">
+					  {{-- <div class="d-flex justify-content-center py-4">
 						<a href="index.html" class="logo d-flex align-items-center w-auto">
-						  <img src="https://www.botire.in/assets/img/logo.png" alt="">
-						  {{-- <span class="d-none d-lg-block">NiceAdmin</span> --}}
+						  <img src="https://www.botire.in/assets/img/logo.png" alt=""> 
+						 <span class="d-none d-lg-block">NiceAdmin</span>
 						</a>
-					  </div><!-- End Logo -->
+					  </div><!-- End Logo --> --}}
 		
 					  <div class="card mb-3">
 		
